836a76a04f3ce5e71b8becbec92595df0b6536c1
EricUnternahr <133649199+EricUnternahr@users.noreply.github.com>
2023-11-10 12:12:15 +0100

    1er push

---
+++ src/main/java/solalim/SolalimApplication.java
@@ -0,0 +1,13 @@
+package solalim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolalimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolalimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/solalim/SolalimApplicationTests.java
@@ -0,0 +1,13 @@
+package solalim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolalimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
